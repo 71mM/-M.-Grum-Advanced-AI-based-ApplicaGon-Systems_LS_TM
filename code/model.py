f72d6f0138bcfe3ac0e0f9ccd0a63b169e7421c2
71mM <97466779+71mM@users.noreply.github.com>
2025-02-01 20:19:29 +0100

    Update model.py

--- code/model.py
+++ code/model.py
@@ -4,7 +4,7 @@ from tensorflow.keras import layers, Model
 class PetalsLSTM(Model):
     def __init__(self, input_size=1, hidden_size=32, num_layers=1):
         super(PetalsLSTM, self).__init__()
-        self.lstm = layers.LSTM(hidden_size, return_sequences=True, recurrent_initializer='glorot_uniform')
+        self.lstm = layers.LSTM(hidden_size, recurrent_initializer='glorot_uniform')
         self.fc = layers.Dense(1)
 
     def call(self, x):
