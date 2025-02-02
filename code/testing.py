0f59d638dbaa0f97bc740eaf80402ce99de65a01
71mM <timmueller@uni-potsdam.de>
2025-02-02 19:01:28 +0100

    implemented the Docker structure from Subgoal 3 and started on subgoal 6

--- code/testing.py
+++ code/testing.py
@@ -1,20 +1,54 @@
 import tensorflow as tf
-from model import PetalsLSTM
+from tensorflow.keras.models import load_model
+from tensorflow.keras import layers, Model
+import pandas as pd
 
-def test_model(model, test_sequence, model_path, max_sequence_length):
+class PetalsLSTM(Model):
+    def __init__(self, input_size=1, hidden_size=32, num_layers=1, **kwargs):
+        super(PetalsLSTM, self).__init__(**kwargs)
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
 
-    model = tf.keras.models.load_model(model_path)
-    padded_sequence = test_sequence + [0] * (max_sequence_length - len(test_sequence))
-    input_tensor = tf.convert_to_tensor([padded_sequence], dtype=tf.float32)
-    prediction = model(input_tensor).numpy().squeeze(0)
+        self.lstm = layers.LSTM(hidden_size, recurrent_initializer='glorot_uniform')
 
-    return prediction[:len(test_sequence)]
+        self.fc = layers.Dense(1)
 
-model = PetalsLSTM(input_size=1, hidden_size=32, num_layers=1)
-test_sequence = [3, 5, 1, 2, 6, 5, 3, 3]
-predicted_petals = test_model(model, test_sequence,"/content/best_model_20250127_140300.pkl")
-true_petals = 14
+    def call(self, x):
+        x = tf.expand_dims(x, axis=-1)
+        lstm_out = self.lstm(x)
+        output = self.fc(lstm_out)
+        return tf.squeeze(output, axis=-1)
 
-predicted_last = round(predicted_petals[-1], 2)
-true_last = true_petals[-1]
+    def get_config(self):
+        config = super(PetalsLSTM, self).get_config()
+        config.update({
+            "input_size": self.input_size,
+            "hidden_size": self.hidden_size,
+            "num_layers": self.num_layers
+        })
+        return config
+
+    @classmethod
+    def from_config(cls, config):
+
+        return cls(**config)
+
+
+
+model = load_model('currentSolution.keras', custom_objects={'PetalsLSTM': PetalsLSTM})
+
+activation_data = pd.read_csv(f'../images/activationBase/activation_data.csv')
+label = activation_data['Label']
+activation_data_input = activation_data.drop(columns=['Label'])
+
+prediction = model.predict(activation_data_input)
+print("Input:")
+print(activation_data_input)
+print("------------------------------------------")
+print("Prediction: ", prediction[0])
+print("actual label: ", label[0])
+
+
+
 
